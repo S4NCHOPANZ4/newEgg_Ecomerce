a7de276a48ab7e5b719f5111b07ccefd23a62cdb
Juan Buitrago <buitr4go@gmail.com>
2023-08-23 22:57:55 -0700

    fix activation email

--- backend/controller/user.js
+++ backend/controller/user.js
@@ -35,7 +35,7 @@ router.post("/create-user", async (req, res, next) => {
 
     const activationToken = createActivationToken(user);
 
-    const activationUrl = `https://eshop-tutorial-pyri.vercel.app/activation/${activationToken}`;
+    const activationUrl = `https://new-egg-ecomerce-y9so.vercel.app/activation/${activationToken}`;
 
     try {
       await sendMail({
